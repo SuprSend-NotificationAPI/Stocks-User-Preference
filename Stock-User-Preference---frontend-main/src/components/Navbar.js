31de4f38a8c8614e8027518e1ce338c731f696aa
SuprSend <129148285+SuprSend-NotificationAPI@users.noreply.github.com>
2023-07-17 20:44:18 +0530

    Add files via upload

---
+++ Stock-User-Preference---frontend-main/src/components/Navbar.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import suprsend from "@suprsend/web-sdk";
+import {useNavigate} from 'react-router-dom';
+import SuprSendInbox from "@suprsend/react-inbox"
+
+export default function Navbar(props) {
+  const port = process.env.REACT_APP_PORT;
+  const [data,setdetails] = React.useState({});
+
+  function Logoutuser(){
+    localStorage.removeItem("token");
+    let navigate = useNavigate();
+    suprsend.reset();
+    setdetails({});
+    navigate("/");
+  }
+
+  const Getdetail = async(req,res)=>{
+     const response = await fetch(`${port}/getuser`,{
+      method : "POST",
+      headers:{
+        "auth-token" : localStorage.getItem('token')
+      },
+     })
+     const json = await response.json();
+     setdetails(json);
+  }
+  
+  React.useEffect(()=>{
+     if(localStorage.getItem('token')){
+      console.log("yes it runs");
+      Getdetail();
+     }
+     else console.log("user not logged in");
+  },[])
+
+
+  return (
+    <div>
+        <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor : "lightgrey"}}>
+        <div className="container-fluid">
+        <Link className="navbar-brand" to="/" style={{fontWeight: "bold"}}>Stock Information</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0" style={{display : 'flex',right : 50,position : 'absolute'}}>
+                {!localStorage.getItem('token')&& <li className= "nav-item"> <Link className="nav-link" style={{fontWeight: "bold",backgroundColor : "lightgrey"}} to="/register">Register</Link></li>}
+                {!localStorage.getItem('token')&& <li className= "nav-item"> <Link className="nav-link" style={{fontWeight: "bold",backgroundColor : "lightgrey"}} to="/login">Login</Link></li>}
+                {localStorage.getItem('token')&& <li className= "nav-item"> <Link className="nav-link" style={{fontWeight: "bold",backgroundColor : "lightgrey"}} to="/" 
+                 onClick={Logoutuser}>Logout</Link></li>}
+                {localStorage.getItem('token')&& <li className= "nav-item"> <Link className="nav-link" style={{fontWeight: "bold",backgroundColor : "lightgrey"}} to="/setting">Settings</Link></li>}
+            </ul>
+            </div>
+        </div>
+        {localStorage.getItem('token')&&<SuprSendInbox
+          workspaceKey= {process.env.REACT_APP_WKEY}
+          subscriberId= {data.key}
+          distinctId= {data.email}
+        />}
+        </nav>
+      </div>
+  )
+}
---
+++ Stock-User-Preference---frontend-main/src/components/stockdata.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function Stockdata(props) {
+  return (
+    <div className='container'>
+      <table className="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">Date</th>
+      <th scope="col">Open</th>
+      <th scope="col">Close</th>
+      <th scope="col">Low</th>
+      <th scope="col">High</th>
+      <th scope="col">Volume</th>
+
+    </tr>
+  </thead>
+  <tbody>
+      {props.Array.map((element)=>{
+        return(
+          <tr key={element.date}>
+          <th scope="row">{element.date}</th>
+              <td>{element["1. open"]}</td>
+              <td>{element["4. close"]}</td>
+              <td>{element["3. low"]}</td>
+              <td>{element["2. high"]}</td>
+              <td>{element["6. volume"]}</td>
+            </tr>
+        )
+      })}
+  </tbody>
+</table>
+    </div>
+  )
+}
